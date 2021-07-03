71b97ec33c84a24239fd39fbb5e484c29d5382c4
Adam Cameron Drummond <66436400+AdamDIOM@users.noreply.github.com>
2021-07-03 23:42:18 +0100

    Add files via upload

---
+++ health app.py
@@ -0,0 +1,8 @@
+print("health hack")
+
+print("How many steps have you done today?")
+
+if int(input()) < 10000:
+    print("go for a walk... you need more steps")
+else:
+    print("Congrats! you are healthy!")
