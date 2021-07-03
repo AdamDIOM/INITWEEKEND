5cca53085a60658555d5b4c89cfd32ca80105fcf
Adam Cameron Drummond <66436400+AdamDIOM@users.noreply.github.com>
2021-07-03 23:59:52 +0100

    Add files via upload

---
+++ productivity hack.py
@@ -0,0 +1,10 @@
+print("productivity hack")
+
+print("are you working right now?")
+
+if input(">_ ") == "yes":
+    print("no you aren't... you're on this app. Are you crazy?")
+else:
+    print("well why not??")
+    input(">_ ")
+    print("pffffft that's not a worthy excuse")
